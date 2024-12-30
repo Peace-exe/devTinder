858bf4b8cb01218f48cd2e1629cd34e2a328186d
Aman Yadav <amanyadav2303@gmail.com>
2024-12-31 05:09:39 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.post("/signup", async (req,res)=>{
         await user.save(); //saving the User instance to our DB //this function returns a promise
         res.send("data saved successfully!!");
     }catch(err){
-        res.status(400).send("error occured while saving to DB "+ err.message);
+        res.status(400).send("error occured while saving to DB. "+ err.message);
     }
     
 
@@ -88,15 +88,47 @@ app.delete("/user",
 );
 
 //update the user info
-app.patch("/user",
+app.patch("/user/:userId",
     async (req,res)=>{
-        const userId= req.body.userId;
+        const userId= req.params?.userId;
         const data = req.body;
+        ALLOWED_UPDATES=[
+            
+            "age",
+            "gender",
+            "photoURL",
+            "about",
+            "skills"
+        ]
         try {
+            /*in below line, Object.keys(data) returns an array of keys present the js object(stored in data) then that array is compared with 
+             * ALLOWED_UPDATES array. lets break it down
+                const dataArray=Object.keys(data);
+                const isUpdateAllowed= dataArray.every(k => ALLOWED_UPDATES.includes(k));
+
+                every() function works on arrays, takes a callback to check if each element in the array pass that check.
+             */
+            const isUpdateAllowed= Object.keys(data).every(k => ALLOWED_UPDATES.includes(k));
+
+            if(!isUpdateAllowed){
+                throw new Error("update not allowed.");
+            }
+
+            if(data?.skills.length<10){
+
+                if(data?.skills.every((skill)=>( skill.length>20 ))){
+                    throw new Error("skills cannot exceed 20 characters.");
+                }
+            }
+            else{
+                throw new Error("Can't include skills more than 10.");
+            }
+
 
             const user = await User.findByIdAndUpdate({_id:userId},data,{
                 /* options */ 
-                returnDocument:"before"
+                returnDocument:"before",
+                runValidators:true
             });// can also write (userId,data)
             res.send("User info updated successfully.")
         } catch (err) {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,73 @@
 const mongoose=require('mongoose');
 
-const userSchema = new mongoose.Schema({                                   //"new" keyword is not importent there
+const userSchema = new mongoose.Schema({ //"new" keyword is not mandatory there
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:1,
+        maxLenght:50
+
     },
     lastName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:1,
+        maxLenght:50
     },
     email:{
-        type:String
+        type:String,
+        required:true,
+        minLength:1,
+        unique:true,
+        trim:true
     },
+    
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","other"].includes(value)){
+                throw new Error("Enter a valid gender.");
+            }
+        }
     },
-});
+    photoURL:{
+        type:String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s"
+    },
+    about:{
+        type:String,
+        default:"this is a default user about"
+    },
+    skills:{
+        type:[String],
+        validate(arr){
+            if(arr.length<10){
+
+                if(arr.every((skill)=>( skill.length>20 ))){
+                    throw new Error("skills cannot exceed 20 characters.");
+                }
+            }
+            else{
+                throw new Error("Can't include skills more than 10.");
+            }
+            
+        }
+
+    }
+
+},
+{
+    timestamps:true
+}
+);
 
 const User = mongoose.model("User",userSchema);
 
