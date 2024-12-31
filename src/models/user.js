837afff93c06da985670fd2f6ea8b9c9566e6544
Aman Yadav <amanyadav2303@gmail.com>
2024-12-31 05:33:46 +0530

    installed validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const validator= require('validator')
 
 const userSchema = new mongoose.Schema({ //"new" keyword is not mandatory there
     firstName:{
@@ -19,12 +20,23 @@ const userSchema = new mongoose.Schema({ //"new" keyword is not mandatory there
         required:true,
         minLength:1,
         unique:true,
-        trim:true
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("invalid email format: "+value);
+            }
+        }
     },
     
     password:{
         type:String,
-        required:true
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password."+
+                    "Password must contain minimum 8 characters,1 lowercase ,1 uppercase and 1 special character");
+            }
+        }
     },
     age:{
         type:Number,
@@ -40,7 +52,12 @@ const userSchema = new mongoose.Schema({ //"new" keyword is not mandatory there
     },
     photoURL:{
         type:String,
-        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s"
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("invalid URL: "+value);
+            }
+        }
     },
     about:{
         type:String,
